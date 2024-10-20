dbde06a960556a6e7369fb89d48281e1fdfb2b1e
evinyanitoz <eyanitoz@gmail.com>
2024-10-20 14:28:31 +0300

    Add project files.

---
+++ SayiTersCevirme/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SayiTersCevirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan bir sayı girmesini iste en az 4 haneli girilen sayıyı tersten ekrana yazdır.
+
+
+            Console.WriteLine("SAYI GİRİNİZ ");
+            string sayi = Console.ReadLine();
+
+
+            for (int i = sayi.Length - 1; i >= 0; i--)
+            {
+                Console.Write(sayi[i].ToString());
+            }
+
+
+
+
+
+        }
+    }
+}
